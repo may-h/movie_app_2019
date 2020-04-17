5e411f8030d25a1dd223bdf1d95648ccd4ae19aa
may-h <hms9210@naver.com>
2020-04-17 18:00:35 +0900

    learning state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  //Component가 갖고 있는 state를 쓰기 위해 클래스 컴포넌트를 사용. 
+  //state 왜 사용해 ?  데이터는 변하니까!
+  //state는 객체야 
+  state = {
+    count: 0
+  }
+
+  add = () => {
+     console.log('add')
+  }
+
+  minus = () => {
+    console.log('minus');
+  }
+
+  render() {
+    return (
+    <div>
+      <h1> The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
